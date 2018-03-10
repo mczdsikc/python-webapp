2c49e26b64687a86b1bb305ca37107a41fb136a4
mczdsikc <mczdsikc@gmail.com>
2018-03-10 12:47:09 -0600

    day07

--- www/handlers.py
+++ www/handlers.py
@@ -1,8 +1,21 @@
 ' url handlers '
 
+import re
+import time
+import json
+import logging
+import hashlib
+import base64
+import asyncio
 from aiohttp import web
+
 from coroweb import get, post
+from models import User, Blog, Comment, next_id
 
 @get('/')
-def index(request):
-    return web.Response(body=b'<h1>Awesome</h1>', content_type='text/html')
+async def index(request):
+    users = await User.findAll()
+    return {
+        '__template__': 'test.html',
+        'users': users
+    }

e9fc51e9c7afb3669f5db65a322c7dc3b0d4f343
mczdsikc <mczdsikc@gmail.com>
2018-03-10 12:04:19 -0600

    day06

---
+++ www/config_default.py
@@ -0,0 +1,17 @@
+'''
+Default configurations.
+'''
+
+configs = {
+    'debug': True,
+    'db': {
+        'host': '127.0.0.1',
+        'port': 3306,
+        'user': 'user1',
+        'password': 'Hg4oHqJPmmbbWBoW',
+        'db': 'user1'
+    },
+    'session': {
+        'secret': 'Awesome'
+    }
+}
